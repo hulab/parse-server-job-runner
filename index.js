c8e1991f1f789aa39870c7de1548a4f745ba7ac0
SebC99 <sebastien.caron@gmail.com>
2019-07-25 17:55:31 +0200

    Remove Parse dependency

--- index.js
+++ index.js
@@ -1,15 +1,11 @@
 #!/usr/bin/env node
-const Parse = require('parse/node');
-const { JobSchedule } = require('./lib/JobSchedule');
+const JobSchedule = require('./lib/JobSchedule');
 
 exports.handler = function(event, context, callback) {
   if (!process.env.PARSE_APPLICATION_ID || !process.env.PARSE_MASTER_KEY || !process.env.PARSE_SERVER_URL) {
     if (callback) callback(new Error('Missing required environment variables (PARSE_APPLICATION_ID, PARSE_MASTER_KEY, PARSE_SERVER_URL)'));
     return;
   }
-
-  Parse.initialize(process.env.PARSE_APPLICATION_ID, undefined, process.env.PARSE_MASTER_KEY);
-  Parse.serverURL = process.env.PARSE_SERVER_URL;
 
   JobSchedule.run()
     .then((result) => {
